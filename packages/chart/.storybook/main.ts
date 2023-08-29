83b88f182e36120eb8d994708c0a5bd38ac771bf
권재원 <springkjw@gmail.com>
2023-08-29 16:28:51 +0900

    [ci/PDS-1]Lerna + Storybook Build 연동

---
+++ packages/chart/.storybook/main.ts
@@ -0,0 +1,8 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import mainConfig from "../../../.storybook/main";
+
+const config: StorybookConfig = {
+  ...mainConfig,
+};
+
+export default config;
---
+++ packages/chart/.storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+      storySort: (a, b) => a.title === b.title ? 0 : a.id.localeCompare(b.id, { numeric: true }),
+    },
+    layout: "fullscreen",
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
