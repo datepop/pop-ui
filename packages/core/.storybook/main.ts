400aaa5bd346c9ad4781e9745c31e954abf019fd
codono <codonopsis0130@gmail.com>
2023-11-09 15:44:39 +0900

    chromatic 적용

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,35 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import mainConfig from "../../../.storybook/main";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    {
-      name: "@storybook/preset-scss",
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-          modules: {
-            mode: "local",
-            auto: true,
-            localIdentName: "[name]__[local]___[hash:base64:5]",
-            exportGlobals: true,
-          },
-        },
-      },
-    },
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-themes",
-    "storybook-addon-mock",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  ...mainConfig,
 };
 
 export default config;
