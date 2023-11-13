c97d7538443b8f2bf96271d0b7349d9126795683
codono <codonopsis0130@gmail.com>
2023-11-13 15:53:43 +0900

    [feat/PDS-3] Button 컴포넌트 수정

--- packages/core/src/components/Button/index.tsx
+++ packages/core/src/components/Button/index.tsx
@@ -1,18 +1,43 @@
-import { Button as MantineButton, ButtonProps as MantineButtonProps } from '@mantine/core';
+import {
+  Button as MantineButton,
+  ButtonProps as MantineButtonProps,
+} from "@mantine/core";
 import styles from "./styles.module.scss";
 
 export interface ButtonProps extends MantineButtonProps {
   onClick?: () => void;
+  size?: "sm" | "md" | "lg";
+  styleType?: "primary" | "primaryline" | "basic" | "danger" | "setting";
 }
 
 export const Button = ({
   children,
+  size = "md",
+  styleType = "primary",
   ...props
 }: ButtonProps) => {
+  let sizeStyle = styles.md_button;
+  if (size === "sm") {
+    sizeStyle = styles.sm_button;
+  } else if (size === "lg") {
+    sizeStyle = styles.lg_button;
+  }
+
+  let buttonStyle = styles.primary;
+  if (styleType === "basic") {
+    buttonStyle = styles.basic;
+  } else if (styleType === "primaryline") {
+    buttonStyle = styles.primaryline;
+  } else if (styleType === "danger") {
+    buttonStyle = styles.danger;
+  } else if (styleType === "setting") {
+    buttonStyle = styles.setting;
+  }
+
   return (
     <MantineButton
       type="button"
-      className={styles.PopButton}
+      className={`${buttonStyle} ${sizeStyle}`}
       {...props}
     >
       {children}
