fefae658296215384c662dee31c0b6b6d5b7db5b
여다희 <yeodahui.dev@gmail.com>
2026-04-07 14:05:11 +0900

    fix(core): 라디오 버튼 사이즈별 레이아웃 오류 수정

--- packages/core/src/components/Radio/index.tsx
+++ packages/core/src/components/Radio/index.tsx
@@ -12,14 +12,29 @@ const RADIO_CLASS_NAME_BY_SIZE = {
   lg: styles['Radio--Large'],
 } as const;
 
+const RADIO_DIMENSION_BY_SIZE = {
+  sm: 18,
+  md: 24,
+  lg: 32,
+} as const;
+
 export interface IRadioProps extends MantineRadioProps {
   size?: 'sm' | 'md' | 'lg';
 }
 
 export const Radio = ({ size = 'md', ...props }: IRadioProps) => {
   const sizeClassName = RADIO_CLASS_NAME_BY_SIZE[size];
-
-  return <MantineRadio className={sizeClassName} size={size} {...props} />;
+  const sizeNumber = RADIO_DIMENSION_BY_SIZE[size];
+  return (
+    <MantineRadio
+      className={sizeClassName}
+      size={size}
+      styles={{
+        inner: { width: sizeNumber, height: sizeNumber },
+      }}
+      {...props}
+    />
+  );
 };
 
 export default Radio;
